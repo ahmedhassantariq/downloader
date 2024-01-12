281588c27a09e1384c186c5d0f5dc3364d9e2a7d
atari <atariq344@gmail.com>
2024-01-12 17:14:37 +0500

    First

---
+++ app/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import json
+import yt_dlp
+from urllib.parse import unquote
+
+app = FastAPI()
+
+
+@app.get("/api/getVideoInfo/")
+async def downloader(url: str):
+    ydl_opts = {}
+    decoded_url = unquote(url)
+
+    print(decoded_url)
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(decoded_url, download=False)
+    return {json.dumps(ydl.sanitize_info(info))}
